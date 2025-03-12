452a732dbc7775deed4aee0bbb7ab1354f6094dc
Thorfilo2612 <ando1661@gmail.com>
2025-03-12 17:10:43 -0400

    Update ClaveDicotomica.java

--- src/proyecto2/ClaveDicotomica.java
+++ src/proyecto2/ClaveDicotomica.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package proyecto2;
+package plants;
 
 /**
  *
- * @author Thony
+ * @author Santiago, Fernando y Anthony
  */
 public class ClaveDicotomica {
     private Arbol arbol;
