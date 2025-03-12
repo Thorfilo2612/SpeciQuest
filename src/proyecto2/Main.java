4a109c3b27cb6a6f9088e053c9ae1f9e0727ace9
Thorfilo2612 <ando1661@gmail.com>
2025-03-12 17:10:17 -0400

    Update and rename Proyecto2.java to Main.java

--- src/proyecto2/Main.java
+++ src/proyecto2/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyecto2;
+package plants;
 
 /**
  *
  * @author Santiago, Fernando y Anthony
  */
-public class Proyecto2 {
+public class Main {
 
     /**
      * @param args the command line arguments
