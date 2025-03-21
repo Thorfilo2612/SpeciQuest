e912afa54ce783d669d0be8eb9be39275fbedd15
Atach0 <167770002+Atach0@users.noreply.github.com>
2025-03-21 15:25:10 -0400

    Create SpecQuest.java
    Archivo Main

---
+++ src/plants/SpecQuest.java
@@ -0,0 +1,40 @@
+package specquest;
+import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author Thony
+ */
+public class SpecQuest extends JFrame{
+
+    public static void main(String[] args) {
+        // Crear árbol de prueba
+        Nodo raiz = new Nodo("¿Tiene hojas verdes?");
+        Nodo nodoSi = new Nodo("¿Es un árbol frutal?");
+        Nodo nodoNo = new Nodo("¿Tiene flores?");
+        Nodo nodoFrutalSi = new Nodo("Manzano");
+        Nodo nodoFrutalNo = new Nodo("Pino");
+        Nodo nodoFlorSi = new Nodo("Rosa");
+        Nodo nodoFlorNo = new Nodo("Cactus");
+
+        raiz.setNodoSi(nodoSi);
+        raiz.setNodoNo(nodoNo);
+        nodoSi.setNodoSi(nodoFrutalSi);
+        nodoSi.setNodoNo(nodoFrutalNo);
+        nodoNo.setNodoSi(nodoFlorSi);
+        nodoNo.setNodoNo(nodoFlorNo);
+
+        Arbol arbol = new Arbol();
+        arbol.setRaiz(raiz);
+
+        TablaHash tabla = new TablaHash();
+        tabla.insertar("Manzano", "Es un árbol frutal");
+        tabla.insertar("Rosa", "Es una flor");
+
+        SwingUtilities.invokeLater(() -> new ClaveDicotomica(arbol));
+    }
+    
+}
