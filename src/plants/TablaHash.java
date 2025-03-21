09a22d6de822fb3293ab1525c6bc1d8aa7679c43
Atach0 <167770002+Atach0@users.noreply.github.com>
2025-03-21 15:26:33 -0400

    Create TablaHash.java
    Tabla hash

---
+++ src/plants/TablaHash.java
@@ -0,0 +1,85 @@
+package specquest;
+
+/**
+ *
+ * @author Santiago Atacho
+ */
+public class TablaHash {
+    private static final int TAMANO = 100;
+    private NodoHash[] tabla;
+
+    private static class NodoHash {
+        String clave;
+        String valor;
+        NodoHash siguiente;
+
+        NodoHash(String clave, String valor) {
+            this.clave = clave;
+            this.valor = valor;
+            this.siguiente = null;
+        }
+    }
+
+    public TablaHash() {
+        tabla = new NodoHash[TAMANO];
+    }
+
+    // Función de hash personalizada
+    private int hash(String clave) {
+        int hash = 0;
+        for (int i = 0; i < clave.length(); i++) {
+            hash = (31 * hash + clave.charAt(i)) % TAMANO;
+        }
+        return hash;
+    }
+
+    // Insertar valor en la tabla
+    public void insertar(String clave, String valor) {
+        int indice = hash(clave);
+        NodoHash nuevo = new NodoHash(clave, valor);
+
+        if (tabla[indice] == null) {
+            tabla[indice] = nuevo;
+        } else {
+            NodoHash actual = tabla[indice];
+            while (actual.siguiente != null) {
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevo;
+        }
+    }
+
+    // Buscar valor en la tabla
+    public String buscar(String clave) {
+        int indice = hash(clave);
+        NodoHash actual = tabla[indice];
+
+        while (actual != null) {
+            if (actual.clave.equals(clave)) {
+                return actual.valor;
+            }
+            actual = actual.siguiente;
+        }
+        return "No encontrado";
+    }
+
+    // Eliminar valor de la tabla
+    public void eliminar(String clave) {
+        int indice = hash(clave);
+        NodoHash actual = tabla[indice];
+        NodoHash previo = null;
+
+        while (actual != null) {
+            if (actual.clave.equals(clave)) {
+                if (previo == null) {
+                    tabla[indice] = actual.siguiente;
+                } else {
+                    previo.siguiente = actual.siguiente;
+                }
+                return;
+            }
+            previo = actual;
+            actual = actual.siguiente;
+        }
+    }
+}
